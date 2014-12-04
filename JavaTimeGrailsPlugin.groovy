3517635a4172ce176e33ef85f42d594c159494a5
Sergey Ponomarev <stokito@gmail.com>
2014-12-04 18:15:00 +0200

    Joda-Time -> Java-Time ;)

---
+++ JavaTimeGrailsPlugin.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Rob Fletcher, Sergey Ponomarev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class JavaTimeGrailsPlugin {
+
+	def version = '0.1-SNAPSHOT'
+	def grailsVersion = '2.4 > *'
+	def dependsOn = [converters: '2.0 > *']
+
+	def title = 'Java-Time Plugin'
+	def description = 'JSR 310 Java Time integration for Grails'
+	def author = 'Sergey Ponomarev'
+	def authorEmail = 'rob@freeside.co'
+
+	def license = 'APACHE'
+	def developers = [[name: 'Sergey Ponomarev', email: 'stokito@gmail.com'], [name: 'Rob Fletcher', email: 'rob@freeside.co', role: 'original author']]
+	def documentation = 'https://github.com/stokito/grails-java-time/'
+	def issueManagement = [system: 'GitHub', url: 'https://github.com/stokito/grails-java-time/issues']
+	def scm = [url: 'https://github.com/stokito/grails-java-time/']
+
+	def pluginExcludes = [
+			'grails-app/controllers/**',
+			'grails-app/domain/**',
+			'grails-app/i18n/**',
+			'web-app/**'
+	]
+
+	def doWithSpring = {
+//		javaTimePropertyEditorRegistrar(JavaTimePropertyEditorRegistrar)
+//
+//		DateTimeConverter.SUPPORTED_TYPES.each{ javaTimeType ->
+//			"${javaTimeType.simpleName}Converter"(DateTimeConverter) {
+//				grailsApplication = ref("grailsApplication")
+//				type = javaTimeType
+//			}
+//		}
+//		"javaTimeZoneConverter"(ZoneIdConverter)
+	}
+
+	def doWithDynamicMethods = { ctx ->
+//		JavaTimeConverters.registerJsonAndXmlMarshallers()
+//		DateTimeStructuredBindingEditor.SUPPORTED_TYPES.each{ type ->
+//			grailsApplication.mainContext.grailsWebDataBinder.registerStructuredEditor type, new DateTimeStructuredBindingEditor(type)
+//		}
+	}
+}
--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-[![Build Status](https://travis-ci.org/gpc/grails-joda-time.svg)](https://travis-ci.org/gpc/grails-joda-time)
+[![Build Status](https://travis-ci.org/stokito/grails-java-time.svg)](https://travis-ci.org/stokito/grails-java-time)
 
-# Joda-Time Plugin
+# Java-Time Plugin
 
-The Joda-Time Plugin integrates the [Joda-Time][1] date/time library into Grails. The plugin...
+The Java-Time Plugin integrates the [Java-Time](http://www.threeten.org/) date/time library into Grails. The plugin...
 
-* Bundles the libraries necessary to use _Joda Time_ types as persistent fields on domain classes.
-* Provides the ability to bind from form inputs to _Joda Time_ fields on domain or command objects.
-* Supports _JSON_ and _XML_ rendering of _Joda Time_ types.
-* Provides tag-libs for input and output of _Joda Time_ data.
-* Enhances Grails' scaffolding to support domain classes with _Joda Time_ fields.
-* Adds compatibility and consistency methods to _Joda Time_ types so that they integrate better with Groovy.
-
-[1]:http://joda-time.sourceforge.net/
+* Bundles the libraries necessary to use _Java Time_ types as persistent fields on domain classes.
+* Provides the ability to bind from form inputs to _Java Time_ fields on domain or command objects.
+* Supports _JSON_ and _XML_ rendering of _Java Time_ types.
